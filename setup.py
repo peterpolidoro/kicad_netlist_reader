e19a5f5899f8d2e604f59c441100b3e01b3a6d2d
Peter Polidoro <peterpolidoro@gmail.com>
2014-12-09 12:50:32 -0500

    Modified classifiers to match official list of classifiers.

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GPL License',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
