18936bddaf9064b8d10f51c6809cee6370763868
Peter Polidoro <peterpolidoro@gmail.com>
2015-08-12 10:48:19 -0400

    Updated homepage url.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/peterpolidoro/kicad_netlist_reader',
+    url='https://github.com/janelia-pypi/kicad_netlist_reader',
 
     # Author details
     author='Jean-Pierre Charras',
