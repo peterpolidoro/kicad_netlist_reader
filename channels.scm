4e8ea5602858cc01fc46dbdac52afc6cdc535270
Peter Polidoro <peter@polidoro.io>
2022-02-20 20:04:25 -0500

    Update to version 5.1.12 and remove pdb import

--- channels.scm
+++ channels.scm
@@ -1,8 +1,9 @@
 (list (channel
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
         (commit
-          "68d15fcf20feddb72f5c2d2ff7d754dcc4f409df")
+          "45995eab3ae62ceef89ee223a1bc805738356c1f")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
--- kicad_netlist_reader/kicad_netlist_reader.py
+++ kicad_netlist_reader/kicad_netlist_reader.py
@@ -20,7 +20,6 @@ from __future__ import print_function
 import sys
 import xml.sax as sax
 import re
-import pdb
 import string
 
 #-----<Configure>----------------------------------------------------------------
